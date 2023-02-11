ef8a3da21661ea51ce2ddfcbfca10def9dccc4c1
Aravind104 <ontagodiaravind@gmail.com>
2023-02-11 16:33:40 +0530

    feat(user):add routes and controller methods to update,delete,get and get all users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -13,23 +13,23 @@ import {
 
 const router = express.Router();
 
-router.get("/checkAuthentication", verifyToken, (req, res, next) => {
-  res.send("hello user,you are logged in");
-});
+// router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+//   res.send("hello user,you are logged in");
+// });
 
-router.get("/checkuser/:userId", verifyUser, (req, res, next) => {
-  res.send("hello user, you are logged in and you can delete your account");
-});
+// router.get("/checkuser/:userId", verifyUser, (req, res, next) => {
+//   res.send("hello user, you are logged in and you can delete your account");
+// });
 
-router.get("/checkadmin/:userId", verifyAdmin, (req, res, next) => {
-  res.send("hello admin, you are logged in and you can delete all accounts");
-});
+// router.get("/checkadmin/:userId", verifyAdmin, (req, res, next) => {
+//   res.send("hello admin, you are logged in and you can delete all accounts");
+// });
 // UPDATE
-// router.put("/:userId", verifyUser, updateUser);
+router.put("/update/:userId", verifyUser, updateUser);
 // DELETE
-// router.delete("/:userId", verifyUser, deleteUser);
+router.delete("/remove/:userId", verifyUser, deleteUser);
 // GET
-// router.get("/:userId", verifyUser, getSingleUser);
+router.get("/get/:userId", verifyUser, getSingleUser);
 // GET ALL
-// router.get("/", verifyAdmin, getAllUsers);
+router.get("/get-all", verifyAdmin, getAllUsers);
 export default router;
