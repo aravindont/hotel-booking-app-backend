dce197755e9b5944a61a7c4cc5862a47f9bc339a
Aravind104 <ontagodiaravind@gmail.com>
2023-02-11 16:04:57 +0530

    chore(auth):auth middleware test

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { customError } from "../utils/customError.js";
+import envConfig from "../config/env.config.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(customError(401, "you are not authenticated!!"));
+  }
+  jwt.verify(token, envConfig.JWT_SECRET, (err, user) => {
+    if (err) return next(customError(403, "Token is not valid!"));
+    req.user = user;
+    next();
+  });
+};
+
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.userId === req.params.userId || req.user.isAdmin) {
+      next();
+    } else {
+      return next(customError(403, "You are not authorized!"));
+    }
+  });
+};
+
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(customError(403, "You are not authorized!"));
+    }
+  });
+};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,35 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+} from "../controllers/users.controller.js";
+import {
+  verifyAdmin,
+  verifyToken,
+  verifyUser,
+} from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-// CREATE
-// UPDATE
-// DELETE
-// GET
-// GET ALL
+router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+  res.send("hello user,you are logged in");
+});
 
+router.get("/checkuser/:userId", verifyUser, (req, res, next) => {
+  res.send("hello user, you are logged in and you can delete your account");
+});
+
+router.get("/checkadmin/:userId", verifyAdmin, (req, res, next) => {
+  res.send("hello admin, you are logged in and you can delete all accounts");
+});
+// UPDATE
+// router.put("/:userId", verifyUser, updateUser);
+// DELETE
+// router.delete("/:userId", verifyUser, deleteUser);
+// GET
+// router.get("/:userId", verifyUser, getSingleUser);
+// GET ALL
+// router.get("/", verifyAdmin, getAllUsers);
 export default router;
