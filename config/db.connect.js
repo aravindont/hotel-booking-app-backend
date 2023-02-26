304756340ba12d671006877f91c74a7cb039b338
Aravind104 <ontagodiaravind@gmail.com>
2023-02-26 16:17:35 +0530

    fix(mongoose):try mongoose error correction

--- config/db.connect.js
+++ config/db.connect.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 export const connectToDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     if (conn) {
       console.log(`connected to ${conn.connection.name}`);
     }
